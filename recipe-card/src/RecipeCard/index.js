1b50a1a0d5539b07e762db2d2509bf528eacecb2
FJada <124617739+FJada@users.noreply.github.com>
2023-09-13 11:54:58 -0400

    in class work

---
+++ recipe-card/src/RecipeCard/index.js
@@ -0,0 +1,8 @@
+import REACT from 'react'
+import RECIPE_IMG from '../assets/Best-Banana-Nut-Bread-Recipe.jpg'
+
+<img src={RECIPE_IMG} alt="Banan nut bread" />
+
+export default function RecipeCard(){
+    
+}
